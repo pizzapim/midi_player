25830345efb788f7d4d71be70b2fcd8fc6d4175e
Pim Kunis <pim@kunis.nl>
2020-10-11 12:27:49 +0200

    Add change program event.

--- lib/midi_tools/event.ex
+++ lib/midi_tools/event.ex
@@ -13,6 +13,14 @@ defmodule MIDITools.Event do
     end
   end
 
+  defmodule ChangeProgram do
+    defstruct channel: 0, time: 0, program: 0
+
+    def new(channel, time, program) do
+      %__MODULE__{channel: channel, time: time, program: program}
+    end
+  end
+
   def convert(%Note{
         channel: channel,
         tone: tone,
@@ -23,5 +31,11 @@ defmodule MIDITools.Event do
     note_on = MIDISynth.Command.note_on(channel, tone, velocity)
     note_off = MIDISynth.Command.note_off(channel, tone)
     [{start_time, note_on}, {end_time, note_off}]
+  end
+
+  def convert(%ChangeProgram{channel: channel, time: time, program: program}) do
+    change_program = MIDISynth.Command.change_program(channel, program)
+
+    [{time, change_program}]
   end
 end
