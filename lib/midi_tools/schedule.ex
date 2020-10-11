a8141f741e6752af8c495dc02641d3b2de39c076
Pim Kunis <pim@kunis.nl>
2020-10-11 12:34:11 +0200

    Fix midi ordering.

--- lib/midi_tools/schedule.ex
+++ lib/midi_tools/schedule.ex
@@ -3,7 +3,7 @@ defmodule MIDITools.Schedule do
     events
     |> Enum.flat_map(&MIDITools.Event.convert/1)
     |> Enum.reduce(%{}, fn {time, midi}, acc ->
-      Map.update(acc, time, midi, &<<midi::binary, &1::binary>>)
+      Map.update(acc, time, midi, &<<&1::binary, midi::binary>>)
     end)
     |> Enum.sort()
   end
